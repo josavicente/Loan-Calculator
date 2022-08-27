f124081bf0dedad210258957cdbe30a2b373d48b
Josa Vicente <jvicenpe@gmail.com>
2022-08-28 00:36:20 +0200

    :sparkles:

---
+++ app.js
@@ -0,0 +1,58 @@
+
+document.getElementById('loan-form').addEventListener('submit', calculateResults);
+
+console.log('Before Calculating...');
+function calculateResults(e) {
+      // UI vars
+      e.preventDefault();
+
+      const amount = document.getElementById('amount');
+      const interest = document.getElementById('interest');
+      const years = document.getElementById('years');
+      const monthlyPayment = document.getElementById('monthly-payment');
+      const totalPayment = document.getElementById('total-payment');
+      const totalInterest = document.getElementById('total-interest');
+
+      const principal = parseFloat(amount.value);
+      const calculatedInterest = parseFloat(interest.value) / 100 / 12;
+      const calculatedPayments = parseFloat(years.value) * 12;
+
+      // Compute monthly payment
+
+      const x = Math.pow(1 + calculatedInterest, calculatedPayments);
+      const monthly = (principal*x*calculatedInterest)/(x-1);
+
+      if (isFinite(monthly)) {
+            monthlyPayment.value = monthly.toFixed(2);
+            totalPayment.value = (monthly * calculatedPayments).toFixed(2);
+            totalInterest.value = ((monthly * calculatedPayments) - principal).toFixed(2);
+      } else {
+            showError('Please check your numbers!');
+      }
+      
+}
+
+function showError(error) {
+
+      // Create a div
+      const errorDiv = document.createElement('div');
+
+      // Get elements
+      const card = document.querySelector('.card');
+      const heading = document.querySelector('.heading');
+
+      errorDiv.className = 'alert alert-danger';
+      // Cretae text node and append
+      errorDiv.appendChild(document.createTextNode(error));
+      
+      // Insert error above heading
+
+      card.insertBefore(errorDiv, heading);
+
+      // Clear error after 5 secons
+      setTimeout(clearError, 3000);
+}
+
+function clearError() {
+      document.querySelector('.alert').remove();
+}
